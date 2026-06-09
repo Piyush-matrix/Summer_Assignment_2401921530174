a7da1e7d6a98781ec92a77ba9a6e5e86b031a485
Piyush Yadav <piyushyxd@gmail.com>
2026-06-09 19:41:32 +0530

    Implement solution for longest substring problem

---
+++ Week_2/Day2/Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l=0,r=0;
+        vector<int> hash(256,-1);
+        int maxlen=0;
+        while(r<size(s)){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l=hash[s[r]]+1;
+                }
+            }
+            int len=r-l+1;
+            maxlen=max(len,maxlen);
+            hash[s[r]]=r;
+            r++;
+        }
+        return maxlen;
+    }
+};
