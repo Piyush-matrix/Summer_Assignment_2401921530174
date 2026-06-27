3a1f886de5878bfa82dbd33249d076691cf5e1cb
Piyush Yadav <piyushyxd@gmail.com>
2026-06-27 17:03:56 +0530

    Add removeNthFromEnd function in Solution class
    Implement a function to remove the nth node from the end of a linked list.

---
+++ Week_3/Day_2/Remove_nth_node_from_end.cpp
@@ -0,0 +1,29 @@
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head==NULL) return head;
+          ListNode* fast=head;
+          ListNode* slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+        }
+        if(fast==NULL){
+            ListNode* temp=head;
+            head=head->next;
+            delete temp;
+            return head;
+        }
+        while(fast->next!=NULL){
+            
+            slow=slow->next;
+            fast=fast->next;
+            
+        } 
+    
+         ListNode* deletenode=slow->next;
+         slow->next=slow->next->next;
+         delete(deletenode);
+         return head;
+    }
+};
